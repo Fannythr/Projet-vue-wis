d8b2b4fd380ebc24f473f5562963df5747afa739
Fannythr <fanny.thire2@gmail.com>
2023-01-09 17:15:43 +0100

    Version Finale

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Projet-vue-wis
+Ce projet est un appel API de l'API Pokemon.
+Sur le site web, il y a le listing des Pokemon et une barre de recherche qui permet de trouver un pokemon en particulieR. Une fois le Pokemon trouvé, ces statistiques apparaissent.
--- pokedex.js
+++ pokedex.js
@@ -1,41 +1,40 @@
-// create a new Vue.js app
-const app = new Vue({
-  // specify the root element for the app
-  el: '#pokedex',
-  // define the data for the app
-  data: {
+// création de Vue.js 
+Vue.createApp({
+  // Définition des données
+  
+  data(){return{
     searchQuery: '',
     pokemon: [],
     selectedPokemon: null
-  },
-  // define a computed property for filtering the list of Pokemon
+  }},
+  // Filtre
   computed: {
     filteredPokemon() {
-      // filter the list of Pokemon based on the search query
+      // filtrer la list des pokemon
       return this.pokemon.filter(pokemon => {
         return pokemon.name.toLowerCase().includes(this.searchQuery.toLowerCase())
       })
     }
   },
-  // define a method for fetching a list of Pokemon from the API
+  // Définir une méthode pour aller chercher la liste des Pokemon via API
   methods: {
     async getPokemonList() {
-      // make a GET request to the Pokemon API to get a list of Pokemon
+      // Faire un GET pour avoir la liste des pokemons
       const response = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=151')
-      // store the list of Pokemon in the data
+      // Liste entière des pokemons
       this.pokemon = response.data.results
     },
-    // define a method for fetching a single Pokemon from the API
+    // Définition d'une méthode pour aller chercher un seul pokemon de l'api
     async getPokemon(url) {
-      // make a GET request to the Pokemon API to get the details of a single Pokemon
+      // Faire un gat pour avoir les détails d'un pokemon
       const response = await axios.get(url)
-      // store the selected Pokemon in the data
+      // Liste des pokemons selectionner
       this.selectedPokemon = response.data
     }
   },
-  // lifecycle hook that is called when the app is created
+  // Utile pour l'application
   created() {
-    // fetch the list of Pokemon when the app is created
     this.getPokemonList()
   }
-})
+}).mount("#pokedex");
+
